a69d94abd44a89d7168ff290b17192a558597ef8
Elias <contact@eliasnau.dev>
2025-12-14 13:24:12 +0100

    feat: add auth package with better auth config

---
+++ packages/auth/src/server.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+import { db } from "@repo/db";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3001",
+	secret: process.env.BETTER_AUTH_SECRET!,
+	plugins: [
+		nextCookies(), //! has to be last plugin in array
+	],
+});
+
+export type Session = typeof auth.$Infer.Session;
