7c997fb1d53dbeb30d70dabdc02cddd4681123eb
Elias <contact@eliasnau.dev>
2025-12-14 14:00:55 +0100

    feat: add passkey auth support

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,7 +1,11 @@
 import { createAuthClient } from "better-auth/react";
+import { passkeyClient } from "@better-auth/passkey/client"
 
 export const authClient = createAuthClient({
 	baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3001",
+	plugins: [
+		passkeyClient()
+	]
 });
 
 export type Session = typeof authClient.$Infer.Session;
--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 import { db } from "@repo/db";
+import { passkey } from "@better-auth/passkey";
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -13,6 +14,7 @@ export const auth = betterAuth({
 	baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3001",
 	secret: process.env.BETTER_AUTH_SECRET!,
 	plugins: [
+		passkey(),
 		nextCookies(), //! has to be last plugin in array
 	],
 });
