360546d3183fa9c4b29e36f447dbe63e7db02331
Elias <contact@eliasnau.dev>
2025-12-14 14:23:49 +0100

    fix: export toNextJsHandler for auth api route

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,3 +1,4 @@
 export { auth } from "./server";
 export type { Session } from "./server";
 export { getSessionCookie } from "better-auth/cookies";
+export { toNextJsHandler } from "better-auth/next-js";
