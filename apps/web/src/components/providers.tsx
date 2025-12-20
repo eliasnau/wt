bfaf652a4ee373dc02f15a6b01f82f12eb12bb10
Elias Nau <contact@eliasnau.dev>
2025-12-20 15:58:56 +0100

    feat: update query provider to use orpc query

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -6,12 +6,12 @@ import { ThemeProvider } from "./theme-provider";
 import { Toaster } from "./ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { queryClient } from "@/utils/orpc";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-	const [queryClient] = useState(() => new QueryClient());
-
 	return (
 		<ThemeProvider
 			attribute="class"
@@ -20,7 +20,9 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 			disableTransitionOnChange
 		>
 			<QueryClientProvider client={queryClient}>
-				<ConvexProvider client={convex}>{children}</ConvexProvider>
+				<ConvexProvider client={convex}>
+					<NuqsAdapter>{children}</NuqsAdapter>
+				</ConvexProvider>
 			</QueryClientProvider>
 			<Toaster richColors />
 		</ThemeProvider>
