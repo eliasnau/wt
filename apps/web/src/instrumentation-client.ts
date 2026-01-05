77836f59bc99fd85042eaae26fc0f81b83bb1684
Elias Nau <contact@eliasnau.dev>
2026-01-05 20:11:08 +0100

    feat: disable automatic feature flag fetching

--- apps/web/src/instrumentation-client.ts
+++ apps/web/src/instrumentation-client.ts
@@ -4,5 +4,5 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: '/ph',
     ui_host: 'https://eu.posthog.com',
     defaults: '2025-11-30',
-
+    advanced_disable_feature_flags: true,
 });
