a3404def5d6b7b8958d55c94a454c3476347d6c2
Elias Nau <contact@eliasnau.dev>
2026-01-05 19:57:09 +0100

    feat: add posthog req rewrites and sourcemap upload

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,11 +1,33 @@
 import type { NextConfig } from "next";
+import { withPostHogConfig } from "@posthog/nextjs-config";
 
 const nextConfig: NextConfig = {
 	typedRoutes: true,
 	reactCompiler: true,
 	experimental: {
 		authInterrupts: true
-	}
+	},
+	async rewrites() {
+		return [
+		  {
+			source: '/ph/static/:path*',
+			destination: 'https://eu-assets.i.posthog.com/static/:path*',
+		  },
+		  {
+			source: '/ph/:path*',
+			destination: 'https://eu.i.posthog.com/:path*',
+		  },
+		]
+	  },
+	  skipTrailingSlashRedirect: true,
 };
 
-export default nextConfig;
+export default withPostHogConfig(nextConfig, {
+	personalApiKey: process.env.POSTHOG_API_KEY!,
+	envId: process.env.POSTHOG_ENV_ID!,
+	host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+	sourcemaps: {
+	  enabled: true, 
+	  deleteAfterUpload: true, // Optional: Delete sourcemaps after upload, defaults to true
+	},
+  });
