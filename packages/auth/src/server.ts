490c71520ebf48cce1687b2616cd92ba0fc458ff
Elias Nau <contact@eliasnau.dev>
2026-01-15 14:02:31 +0100

    feat: add auth caching and ratelimiting using kv

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -6,11 +6,29 @@ import { nextCookies } from "better-auth/next-js";
 import { organization, twoFactor } from "better-auth/plugins";
 import { ac, admin, member, owner } from "./permissions";
 import { manageSessions } from "./plugins/manageSessions";
+import { redis } from "@repo/db/kv";
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: "pg",
 	}),
+	secondaryStorage: {
+		get: async (key) => {
+			return await redis.get(key);
+		},
+		set: async (key, value, ttl) => {
+			if (ttl) await redis.set(key, value, { ex: ttl });
+			else await redis.set(key, value);
+		},
+		delete: async (key) => {
+			await redis.del(key);
+		}
+	},
+	rateLimit: {
+		storage: "secondary-storage",
+		window: 60,
+        max: 100,
+	},
 	emailAndPassword: {
 		enabled: true,
 	},
