deb060e08c24bacbdef6db4d7c17c3bceb53fdd9
Elias Nau <contact@eliasnau.dev>
2026-02-20 20:13:31 +0100

    feat: change auth id generation to use prefixes (e.g. user_, org_)

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -1,4 +1,5 @@
 import { passkey } from "@better-auth/passkey";
+import { createId } from "@paralleldrive/cuid2";
 import { db } from "@repo/db";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { betterAuth } from "better-auth/minimal";
@@ -8,81 +9,104 @@ import { ac, admin, member, owner } from "./permissions";
 import { manageSessions } from "./plugins/manageSessions";
 
 export const auth = betterAuth({
-	database: drizzleAdapter(db, {
-		provider: "pg",
-	}),
-	// secondaryStorage: {
-	// 	get: async (key) => {
-	// 		return await redis.get(key);
-	// 	},
-	// 	set: async (key, value, ttl) => {
-	// 		if (ttl) await redis.set(key, value, { ex: ttl });
-	// 		else await redis.set(key, value);
-	// 	},
-	// 	delete: async (key) => {
-	// 		await redis.del(key);
-	// 	}
-	// },
-	// rateLimit: {
-	// 	storage: "secondary-storage",
-	// 	window: 60,
-	//     max: 100,
-	// },
-	emailAndPassword: {
-		enabled: true,
-	},
-	user: {
-		additionalFields: {
-			hideSensitiveInformatoin: {
-				type: "boolean",
-				required: false,
-				defaultValue: false,
-			},
-		},
-	},
-	session: {
-		cookieCache: {
-			enabled: true,
-			maxAge: 3 * 60,
-		},
-	},
-	baseURL:
-		process.env.BETTER_AUTH_URL ||
-		process.env.VERCEL_URL ||
-		"http://localhost:3001",
-	trustedOrigins: [process.env.VERCEL_URL!, process.env.BETTER_AUTH_URL!],
-	secret: process.env.BETTER_AUTH_SECRET!,
-	emailVerification: {
-		sendVerificationEmail: async ({ url }) => {
-			console.log("Email Verification: ", url);
-		},
-	},
-	plugins: [
-		passkey(),
-		organization({
-			ac,
-			roles: {
-				owner,
-				admin,
-				member,
-			},
-			disableOrganizationDeletion: true,
-			dynamicAccessControl: {
-				enabled: true,
-			},
-			membershipLimit: 15,
-		}),
-		twoFactor({
-			issuer: "WT",
-		}),
-		manageSessions(),
-		haveIBeenPwned({
-			customPasswordCompromisedMessage:
-				"This password has been found in a Data breach. Please choose a more secure one",
-		}),
-		nextCookies(), //! has to be last plugin in array
-	],
-	experimental: { joins: true },
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  advanced: {
+    database: {
+      generateId: (options) => {
+        if (options.model === "user" || options.model === "users") {
+          return `user_${createId()}`;
+        }
+
+        if (options.model === "organization") {
+          return `org_${createId()}`;
+        }
+
+        if (options.model === "session") {
+          return `sess_${createId()}`;
+        }
+
+        if (options.model === "account") {
+          return `acct_${createId()}`;
+        }
+
+        return createId();
+      },
+    },
+  },
+  // secondaryStorage: {
+  // 	get: async (key) => {
+  // 		return await redis.get(key);
+  // 	},
+  // 	set: async (key, value, ttl) => {
+  // 		if (ttl) await redis.set(key, value, { ex: ttl });
+  // 		else await redis.set(key, value);
+  // 	},
+  // 	delete: async (key) => {
+  // 		await redis.del(key);
+  // 	}
+  // },
+  // rateLimit: {
+  // 	storage: "secondary-storage",
+  // 	window: 60,
+  //     max: 100,
+  // },
+  emailAndPassword: {
+    enabled: true,
+  },
+  user: {
+    additionalFields: {
+      hideSensitiveInformatoin: {
+        type: "boolean",
+        required: false,
+        defaultValue: false,
+      },
+    },
+  },
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 3 * 60,
+    },
+  },
+  baseURL:
+    process.env.BETTER_AUTH_URL ||
+    process.env.VERCEL_URL ||
+    "http://localhost:3001",
+  trustedOrigins: [process.env.VERCEL_URL!, process.env.BETTER_AUTH_URL!],
+  secret: process.env.BETTER_AUTH_SECRET!,
+  emailVerification: {
+    sendVerificationEmail: async ({ url }) => {
+      console.log("Email Verification: ", url);
+    },
+  },
+  plugins: [
+    passkey(),
+    organization({
+      ac,
+      roles: {
+        owner,
+        admin,
+        member,
+      },
+      disableOrganizationDeletion: true,
+      dynamicAccessControl: {
+        enabled: true,
+      },
+      membershipLimit: 15,
+    }),
+    twoFactor({
+      issuer: "WT",
+    }),
+    manageSessions(),
+    haveIBeenPwned({
+      customPasswordCompromisedMessage:
+        "This password has been found in a Data breach. Please choose a more secure one",
+    }),
+    nextCookies(), //! has to be last plugin in array
+  ],
+  experimental: { joins: true },
 });
 
 export type Session = typeof auth.$Infer.Session;
