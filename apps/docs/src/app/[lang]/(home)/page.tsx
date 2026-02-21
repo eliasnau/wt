ae640295e27c0b54ca36aa11c7d100c843af1cd0
Elias Nau <contact@eliasnau.dev>
2026-02-21 18:37:27 +0100

    feat: add /docs redirect

--- apps/docs/src/app/[lang]/(home)/page.tsx
+++ apps/docs/src/app/[lang]/(home)/page.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-	return (
-		<div className="flex flex-1 flex-col justify-center text-center">
-			<h1 className="mb-4 font-bold text-2xl">Hello World</h1>
-			<p>
-				You can open{" "}
-				<Link href="/docs" className="font-medium underline">
-					/docs
-				</Link>{" "}
-				and see the documentation.
-			</p>
-		</div>
-	);
+  return redirect("/docs");
+  return (
+    <div className="flex flex-1 flex-col justify-center text-center">
+      <h1 className="mb-4 font-bold text-2xl">Hello World</h1>
+      <p>
+        You can open{" "}
+        <Link href="/docs" className="font-medium underline">
+          /docs
+        </Link>{" "}
+        and see the documentation.
+      </p>
+    </div>
+  );
 }
